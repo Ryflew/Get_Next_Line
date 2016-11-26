26544166fb7355dd599cd4e17cc9c2e603db6897
Victor DARMAYAN <vdarmaya@e3r6p20.42.fr>
2016-11-26 03:09:33 +0100

    remove useless include

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,13 @@
 /*   By: vdarmaya <vdarmaya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/20 00:22:15 by vdarmaya          #+#    #+#             */
-/*   Updated: 2016/11/21 00:32:55 by vdarmaya         ###   ########.fr       */
+/*   Updated: 2016/11/26 03:09:06 by vdarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <string.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include "libft/include/libft.h"
